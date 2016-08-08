61d19ab5462903d70c7e354cb9366c5335fe5b5e
beam018 <beam018@gmail.com>
2016-08-08 12:34:25 +0300

    fix webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        loader: ['babel-loader'],
+        loaders: ['babel-loader']
       }
     ]
   },
