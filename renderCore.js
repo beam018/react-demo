a23040ab375d46979a0b4481bb73acb5d5a0a69e
beam018 <beam018@gmail.com>
2016-08-22 18:01:24 +0300

    fix server rendering

--- renderCore.js
+++ renderCore.js
@@ -2,6 +2,4 @@ import { renderToString } from 'react-dom/server'
 
 import core from './front/core'
 
-// TODO: fix server rendering for routes
-// export default renderToString(core)
-export default ''
+export default renderToString(core)
